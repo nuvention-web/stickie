35adb22deb7c8fb5d67b7f31daace9f5ed4dcb1a
szeng2013 <stephenzeng2013@u.northwestern.edu>
2014-01-14 20:53:40 -0600

    Stickie app initial project

---
+++ Stickie/Stickie/STKViewController.h
@@ -0,0 +1,13 @@
+//
+//  STKViewController.h
+//  Stickie
+//
+//  Created by Stephen Z on 1/14/14.
+//  Copyright (c) 2014 Stephen Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STKViewController : UIViewController
+
+@end
