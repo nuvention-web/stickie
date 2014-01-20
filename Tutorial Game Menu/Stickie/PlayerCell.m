d87626a2d2e3bbde6f38b55696b07d13eec4b452
szeng2013 <stephenzeng2013@u.northwestern.edu>
2014-01-20 14:10:57 -0600

    Tutorial Game

---
+++ Tutorial Game Menu/Stickie/PlayerCell.m
@@ -0,0 +1,13 @@
+//
+//  PlayerCell.m
+//  Stickie
+//
+//  Created by Stephen Z on 1/15/14.
+//  Copyright (c) 2014 Stephen Z. All rights reserved.
+//
+
+#import "PlayerCell.h"
+
+@implementation PlayerCell
+
+@end
---
+++ Tutorial Game Menu/Stickie/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Stickie
+//
+//  Created by Stephen Z on 1/14/14.
+//  Copyright (c) 2014 Stephen Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "STKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([STKAppDelegate class]));
+    }
+}
