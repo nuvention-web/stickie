f24db5d7cd8a35720be2a1b759c912b88da86bb5
Grant Sheldon <Grant@grantshldonsmbp.rae-am3.com>
2014-01-28 12:18:00 -0600

    Updated test file

--- Stickie/StickieTests/SKAssetURLTagMapTest.m
+++ Stickie/StickieTests/SKAssetURLTagMapTest.m
@@ -2,13 +2,22 @@
 //  SKAssetURLTagMapTest.m
 //  Stickie
 //
-//  Created by Grant Sheldon on 1/28/14.
-//  Copyright (c) 2014 Stephen Z. All rights reserved.
+//  Created by Grant Sheldon on 1/24/14.
+//  Copyright (c) 2014 Stickie Inc. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
+#import "SKAssetURLTagMap.h"
+#import "SKImageTag.h"
+#import <AssetsLibrary/AssetsLibrary.h>
+#import <Foundation/Foundation.h>
+
+//#undef NSLog
 
 @interface SKAssetURLTagMapTest : XCTestCase
+
+typedef void (^ALAssetsLibraryGroupsEnumerationResultsBlock)(ALAssetsGroup *group, BOOL *stop);
+typedef void (^ALAssetsLibraryAccessFailureBlock)(NSError *error);
 
 @end
 
@@ -26,9 +35,38 @@
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testSharedInstance
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    SKAssetURLTagMap *map = [SKAssetURLTagMap sharedInstance];
+    XCTAssertNotNil(map, "Singleton should not be nil at this point.");
+    
+    SKImageTag *tag = [[SKImageTag alloc] init];
+    tag.tagName = @"cody";
+    
+    [map setTag:tag forAssetURL: [[NSURL alloc]  initFileURLWithPath: @"www.google.com"]];
+    
+    SKAssetURLTagMap *map2 = [SKAssetURLTagMap sharedInstance];
+    XCTAssertEqualObjects(map, map2, "SKAssetURLTagMap objects should be the same.");
+    
+    map2 = nil;
+    XCTAssertNotEqualObjects(map, map2, "Objects should not be equal at this point.");
+}
+
+-(void)testGetTagForAssetURL
+{
+    SKAssetURLTagMap *map = [SKAssetURLTagMap sharedInstance];
+    
+    SKImageTag *tag = [[SKImageTag alloc] init];
+    tag.tagName = @"cody";
+    
+    NSString *filePath = @"http://www.myschool.edu/~myuserid/test.rtf";
+	NSURL *url = [[NSURL alloc]  initFileURLWithPath:filePath];
+    
+    [map.assetURLToTagMap setObject: tag forKey: url];
+    
+    NSString *tagOutput = [map getTagForAssetURL: url].tagName ;
+    
+    XCTAssertEqualObjects(tag.tagName, tagOutput, "Tags should be equal.");
 }
 
 @end
