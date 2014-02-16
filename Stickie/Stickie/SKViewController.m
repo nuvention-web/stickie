5c99744ed8a62461f6338b93c0063459517aaabd
szeng2013 <stephenzeng2013@u.northwestern.edu>
2014-02-16 15:47:25 -0600

    Drag and drop is almost there.

--- Stickie/Stickie/SKViewController.m
+++ Stickie/Stickie/SKViewController.m
@@ -13,10 +13,17 @@
 #import "SKAssetURLTagsMap.h"
 
 
-@interface SKViewController ()<UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout,UINavigationControllerDelegate, UIImagePickerControllerDelegate>
+@interface SKViewController ()<UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout,UINavigationControllerDelegate, UIImagePickerControllerDelegate,UIGestureRecognizerDelegate>{
+    SKPhotoCell *dCell;
+    NSIndexPath *dIndexPath;
+    UIImage *dImage;
+//    UIImageView *dNewImageView;
+}
 
 @property(nonatomic, weak) IBOutlet UICollectionView *collectionView;
 @property(nonatomic, strong) NSArray *assets;
+@property (strong, nonatomic) IBOutlet UIImageView *dNewImageView;
+
 
 @end
 
@@ -61,6 +68,10 @@
     } failureBlock:^(NSError *error) {
         NSLog(@"Error loading images %@", error);
     }];
+    UILongPressGestureRecognizer *longGestureRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longGestureRecognized:)];
+    longGestureRecognizer.minimumPressDuration = 0.5;
+    longGestureRecognizer.delegate = self;
+    [self.collectionView addGestureRecognizer:longGestureRecognizer];
 }
 
 - (void)didReceiveMemoryWarning
@@ -83,7 +94,6 @@
     
     ALAsset *asset = self.assets[indexPath.row];
     cell.asset = asset;
-//    cell.backgroundColor = [UIColor redColor];
     
     return cell;
 }
@@ -97,33 +107,88 @@
 {
     return 1;
 }
+//-(void)collectionView:(UICollectionView *)collectionView didHighlightItemAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    UILongPressGestureRecognizer *longGestureRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longGestureRecognized:)];
+//    longGestureRecognizer.delaysTouchesBegan = YES;
+//    longGestureRecognizer.minimumPressDuration = 0.5;
+//    longGestureRecognizer.delegate = self;
+//    ALAsset *asset = self.assets[indexPath.row];
+//    UIImage *image = [UIImage imageWithCGImage:[asset thumbnail]];
+//    UIImageView *newImageView = [[UIImageView alloc] initWithImage:image];
+//    [newImageView setUserInteractionEnabled:YES];
+//    [newImageView addGestureRecognizer:longGestureRecognizer];
+//}
 //Select image
 //- (void) collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath
 //{
+//    UILongPressGestureRecognizer *longGestureRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longGestureRecognized:)];
+//    longGestureRecognizer.delaysTouchesBegan = YES;
+//    longGestureRecognizer.minimumPressDuration = 0.5;
+//    longGestureRecognizer.delegate = self;
 //    ALAsset *asset = self.assets[indexPath.row];
-//    NSLog(@"%@",[asset valueForProperty:ALAssetPropertyAssetURL]);
-//    NSURL *url = [asset valueForProperty:ALAssetPropertyAssetURL];
-//    SKAssetURLTagsMap *map = [SKAssetURLTagsMap sharedInstance];
-//    [map removeAllTags];
-//    SKImageTag *tag = [[SKImageTag alloc] initWithName:@"stick" andColor: nil];
-//    [map setTag:tag forAssetURL:url];
-//    NSLog(@"%@", [[map getTagForAssetURL:url] tagName]);
-
-//    ALAssetRepresentation *defaultRep = [asset defaultRepresentation];
-//    UIImage *image = [UIImage imageWithCGImage:[defaultRep fullScreenImage] scale:[defaultRep scale] orientation:0];
+//////    NSLog(@"%@",[asset valueForProperty:ALAssetPropertyAssetURL]);
+//////    NSURL *url = [asset valueForProperty:ALAssetPropertyAssetURL];
+//////    SKAssetURLTagsMap *map = [SKAssetURLTagsMap sharedInstance];
+//////    [map removeAllTags];
+//////    SKImageTag *tag = [[SKImageTag alloc] initWithName:@"stick" andColor: nil];
+//////    [map setTag:tag forAssetURL:url];
+//////    NSLog(@"%@", [[map getTagForAssetURL:url] tagName]);
+////
+//////    ALAssetRepresentation *defaultRep = [asset defaultRepresentation];
+//////    UIImage *image = [UIImage imageWithCGImage:[defaultRep fullScreenImage] scale:[defaultRep scale] orientation:0];
+//    UIImage *image = [UIImage imageWithCGImage:[asset thumbnail]];
 //    UIImageView *newImageView = [[UIImageView alloc] initWithImage:image];
 //    [newImageView setUserInteractionEnabled:YES];
-//    UILongPressGestureRecognizer *longGestureRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longGestureRecognized:)];
+////    UILongPressGestureRecognizer *longGestureRecognizer = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(longGestureRecognized:)];
+////    [newImageView addGestureRecognizer:longGestureRecognizer];
+////    longGestureRecognizer.minimumPressDuration = 0.3;
+//
 //    [newImageView addGestureRecognizer:longGestureRecognizer];
-//    longGestureRecognizer.minimumPressDuration = 0.3;
-
 //}
-
-//-(void)longGestureRecognized:(UILongPressGestureRecognizer *)gestureRecognizer{
+-(void)longGestureRecognized:(UILongPressGestureRecognizer *)gestureRecognizer{
+    gestureRecognizer.delaysTouchesBegan = YES;
+    CGPoint newPoint = [gestureRecognizer locationInView:self.collectionView];
+//    NSIndexPath *indexPath = [self.collectionView indexPathForItemAtPoint:newPoint];
+//    if (indexPath == nil){
+//        NSLog(@"Couldn't find index path");
+//    }
+//    else {
+//        SKPhotoCell* cell = [self.collectionView cellForItemAtIndexPath:indexPath];
+//        UIImage *image = [UIImage imageWithCGImage:[cell.asset thumbnail]];
+//        UIImageView *newImageView = [[UIImageView alloc] initWithImage:image];
+//        [newImageView setCenter:newPoint];
+//        
+//    }
+    
+    switch (gestureRecognizer.state) {
+        case UIGestureRecognizerStateBegan:
+            dIndexPath = [self.collectionView indexPathForItemAtPoint:newPoint];
+            if (dIndexPath == nil){
+                NSLog(@"Couldn't find index path");
+            }
+            dCell = (SKPhotoCell *)[self.collectionView cellForItemAtIndexPath:dIndexPath];
+            dImage = [UIImage imageWithCGImage:[dCell.asset thumbnail]];
+            _dNewImageView = [[UIImageView alloc] initWithImage:dImage];
+            [_dNewImageView setUserInteractionEnabled:YES];
+//            [[self view] bringSubviewToFront:[_dNewImageView superview]];
+//            [[_dNewImageView superview] bringSubviewToFront:_dNewImageView];
+            NSLog(@"Yo I'm in start");
+            break;
+        case UIGestureRecognizerStateChanged:
+            [_dNewImageView setCenter:newPoint];
+            NSLog(@"Yo I'm in middle");
+            break;
+        case UIGestureRecognizerStateEnded:
+            NSLog(@"Yo I'm in end");
+            break;
+        default:
+            break;
+    }
 //    CGPoint newPoint = [gestureRecognizer locationInView:[self view]];
 //    [[self view] bringSubviewToFront:[gestureRecognizer view]];
 //    [[gestureRecognizer view] setCenter:newPoint];
-//}
+}
 
 //Take photo
 //- (IBAction)takePhotoButtonTapped:(id)sender
